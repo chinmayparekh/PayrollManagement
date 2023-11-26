37a269a6c61d83ba509fd5af04fb33429149af6c
Darshak11 <Darshak.Jivrajani@iiitb.org>
2023-11-26 18:56:38 +0530

    Rectified Modification in Designation

--- payroll-management-system.py
+++ payroll-management-system.py
@@ -118,45 +118,39 @@ def modif1(file_name):
         rename('temporary.txt', 'employee_file.txt')
 
 # Modification in Designation
-def modif2():
-    fin = open('employee_file.txt', 'r')
+def modif2(file_name):
+    fin = open(file_name, 'r')
     fout = open('temporary.txt', 'w')
-    # Input Employee Number
     no = input('Employee Number to change Designation? ')
     found = 0
     for data in fin:
         data = data.strip()
         arr = data.split(',')
-        if int(arr[0]) == no:
+        if int(arr[0]) == int(no):
             found = 1
             print('Name :', arr[1])
             print('Designation:', arr[5], '\t\t', 'Basic Salary :', arr[6])
             newdes = input('New Designation? ')
             newbs = input('New Basic Salary? ')
-            if not newdes.isalnum() or newdes.isdigit():
-                print('Please enter proper Designation')
+            while not newdes.isalpha() or not newbs.isdigit():
+                print('Please enter proper Designation and Basic Salary')
                 newdes = input('New Designation? ')
-            elif newdes.isalpha():
-                print('Please enter proper Designation')
-                newdes = input('New Designation? ')
-            else:
-                break
+                newbs = input('New Basic Salary? ')
             print('Are you sure you want to change:\n\tY/y for Yes or N/n for No')
             ch = input('Enter your Choice [Y/y or N/n]? ')
             if ch == 'Y' or ch == 'y':
                 arr[5] = newdes.upper()
                 arr[6] = newbs
                 print('DESIGNATION UPDATED SUCCESSFULLY!\n')
-            else:
-                print('DESIGNATION NOT UPDATED!\n')
         rec = (',').join(arr)
         fout.write(rec + '\n')
     if found == 0:
         print('\nERROR: EMPLOYEE NUMBER NOT FOUND!\n')
     fin.close()
     fout.close()
-    remove('employee_file.txt')
-    rename('temporary.txt', 'employee_file.txt')
+    if found == 1:
+        remove(file_name)
+        rename('temporary.txt', file_name)
     
 
 # Modification in Gender
@@ -720,7 +714,7 @@ while True:
             if a == 1:
                 modif1("employee_file.txt")
             elif a == 2:
-                modif2()
+                modif2("employee_file.txt")
             elif a == 3:
                 modif3()
             elif a == 4:
