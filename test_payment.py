0986b05ad528df408dcac0680af4c6070f4fcc07
Darshak11 <Darshak.Jivrajani@iiitb.org>
2023-11-27 15:45:55 +0530

    Created test cases for modifying designation

--- test_payment.py
+++ test_payment.py
@@ -1,5 +1,5 @@
 import pytest
-from payment import modif1,display_emp
+from payment import modif1,display_emp,modif2
 import os
 def test_modif1_yes(monkeypatch, capfd):
     # Mock the input function
@@ -88,6 +88,113 @@ def test_modif1_empty_file(monkeypatch, capfd):
     # Clean up the test files
     os.remove('test_empty_file.txt')
     os.remove('test_temporary.txt')
+
+def test_modif2_yes(monkeypatch, capfd):
+    # Mock the input function
+    inputs = iter(['1', 'Manager', '6000', 'Y'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    # Create a temporary file with test data
+    with open('test_employee_file.txt', 'w') as f:
+        f.write('1,John,M,1990,2020,ENGINEER,5000,1234567890,9876543210,123 Street\n')
+
+    # Call the function with the test file
+    modif2('test_employee_file.txt', 'test_temporary.txt')
+
+    # Check the contents of the temporary file
+    with open('test_employee_file', 'r') as f:
+        data = f.read()
+    assert '1,John,M,1990,2020,MANAGER,6000,1234567890,9876543210,123 Street\n' in data
+
+    # Check the print statements
+    out, err = capfd.readouterr()
+    assert 'Are you sure you want to change:\n\tY/y for Yes or N/n for No' in out
+    assert 'DESIGNATION UPDATED SUCCESSFULLY!\n' in out
+
+    # Clean up the test files
+    os.remove('test_employee_file.txt')
+
+def test_modif2_no(monkeypatch, capfd):
+    # Mock the input function
+    inputs = iter(['1', 'Manager', '6000', 'N'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    # Create a temporary file with test data
+    with open('test_employee_file.txt', 'w') as f:
+        f.write('1,John,M,1990,2020,ENGINEER,5000,1234567890,9876543210,123 Street\n')
+
+    # Call the function with the test file
+    modif2('test_employee_file.txt', 'test_temporary.txt')
+
+    # Check the contents of the temporary file
+    with open('test_employee_file.txt', 'r') as f:
+        data = f.read()
+    assert '1,John,M,1990,2020,ENGINEER,5000,1234567890,9876543210,123 Street\n' in data
+
+    # Check the print statements
+    out, err = capfd.readouterr()
+    assert 'Are you sure you want to change:\n\tY/y for Yes or N/n for No' in out
+
+    # Clean up the test files
+    os.remove('test_employee_file.txt')
+
+def test_modif2_invalid_input(monkeypatch, capfd):
+    # Mock the input function
+    inputs = iter(['1', '123', 'Manager', 'Y'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    # Create a temporary file with test data
+    with open('test_employee_file.txt', 'w') as f:
+        f.write('1,John,M,1990,2020,ENGINEER,5000,1234567890,9876543210,123 Street\n')
+
+    # Call the function with the test file
+    modif2('test_employee_file.txt', 'test_temporary.txt')
+
+    # Check the print statements
+    out, err = capfd.readouterr()
+    assert 'Please enter proper Designation and Basic Salary' in out
+
+    # Clean up the test files
+    os.remove('test_employee_file.txt')
+
+def test_modif2_employee_not_found(monkeypatch, capfd):
+    # Mock the input function
+    inputs = iter(['2', 'Manager', '6000', 'Y'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    # Create a temporary file with test data
+    with open('test_employee_file.txt', 'w') as f:
+        f.write('1,John,M,1990,2020,ENGINEER,5000,1234567890,9876543210,123 Street\n')
+
+    # Call the function with the test file
+    modif2('test_employee_file.txt', 'test_temporary.txt')
+
+    # Check the print statements
+    out, err = capfd.readouterr()
+    assert '\nERROR: EMPLOYEE NUMBER NOT FOUND!\n' in out
+
+    # Clean up the test files
+    os.remove('test_employee_file.txt')
+
+def test_modif2_empty_file(monkeypatch, capfd):
+    # Mock the input function
+    inputs = iter(['1', 'Manager', '6000', 'Y'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    # Create an empty file
+    open('test_empty_file.txt', 'w').close()
+
+    # Call the function with the empty file
+    modif2('test_empty_file.txt', 'test_temporary.txt')
+
+    # Check the print statements
+    out, err = capfd.readouterr()
+    assert '\nERROR: EMPLOYEE NUMBER NOT FOUND!\n' in out
+
+    # Clean up the test files
+    os.remove('test_empty_file.txt')
+    os.remove('test_temporary.txt')
+
 
 import re
 
